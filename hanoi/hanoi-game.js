8f2527823443496e2f9e1ed3dea2c6e7a40c553c
Anna <anna.arsentieva@gmail.com>
2020-04-30 13:46:28 -0700

    implemented the hanoi game following the unit tests/ specs

--- hanoi/hanoi-game.js
+++ hanoi/hanoi-game.js
@@ -1,11 +1,65 @@
 class HanoiGame {
-  constructor() {}
+  constructor(towers) {
+    if (towers === undefined) {
+      this.towers = [[3, 2, 1], [], []];
+    } else {
+      this.towers = towers;
+    }
+  }
 
-  isValidMove(startTowerIdx, endTowerIdx) {}
+  isValidMove(startTowerIdx, endTowerIdx) {
+    if (startTowerIdx === endTowerIdx) {
+      return false;
+    }
+    let fromTower = this.towers[startTowerIdx];
+    let toTower = this.towers[endTowerIdx];
 
-  move(startTowerIdx, endTowerIdx) {}
+    // console.log(fromTower, toTower);
+    if (fromTower === undefined || toTower === undefined) {
+      return false;
+    }
 
-  isWon() {}
+    if (fromTower.length === 0) {
+      return false;
+    }
+    if (toTower.length === 0) {
+      return true;
+    }
+
+    if (fromTower[fromTower.length - 1] < toTower[toTower.length - 1]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  move(startTowerIdx, endTowerIdx) {
+    if (this.isValidMove(startTowerIdx, endTowerIdx)) {
+      let fromTower = this.towers[startTowerIdx];
+      let toTower = this.towers[endTowerIdx];
+      let moveDisk = fromTower.pop();
+      if (moveDisk !== undefined) {
+        let arrayLength = toTower.push(moveDisk);
+        if (arrayLength > 0) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  isWon() {
+    if (
+      (this.towers[0].length === 0 &&
+        this.towers[this.towers.length - 1].length === 0) ||
+      (this.towers[0].length === 0 &&
+        this.towers[this.towers.length - 2].length === 0)
+    ) {
+      return true;
+    }
+    return false;
+  }
 
   // the below methods are complete and do not need to be modified
   print() {
@@ -15,9 +69,9 @@ class HanoiGame {
 
   promptMove(reader, callback) {
     this.print();
-    reader.question("Enter a starting tower: ", start => {
+    reader.question("Enter a starting tower: ", (start) => {
       const startTowerIdx = parseInt(start);
-      reader.question("Enter an ending tower: ", end => {
+      reader.question("Enter an ending tower: ", (end) => {
         const endTowerIdx = parseInt(end);
         callback(startTowerIdx, endTowerIdx);
       });
@@ -44,5 +98,9 @@ class HanoiGame {
     });
   }
 }
+
+// game = new HanoiGame([[3], [1], [2]]);
+// console.log("towers", game.towers);
+// console.log(game.isValidMove(0, 1));
 
 module.exports = HanoiGame;
